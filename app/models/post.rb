fec1c47d26b6fdbf5c6881e94dbfcfa2d9661fe0
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-05 10:30:13 +0900

    エラー修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
 
-  belong_to :user
+  belongs_to :user
 end
