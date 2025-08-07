d4b53eb09600a551d558bd73645776fb2e4da914
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-07 20:57:15 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "top#index"
-  resources :posts, only: %i[index new create edit update]
+  resources :posts, only: %i[index new create show edit update]
 end
