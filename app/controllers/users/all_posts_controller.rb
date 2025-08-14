c818cfc14d37ee3a0ad758bd7dfc10c9491f7612
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-14 23:56:15 +0900

    当日分のみアルバムで表示、ヘッダーにアルバム表示用のリンク追加

--- app/controllers/users/all_posts_controller.rb
+++ app/controllers/users/all_posts_controller.rb
@@ -2,6 +2,6 @@ class Users::AllPostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.includes(:user) # 全ユーザーの投稿を表示
+    @posts = Post.includes(:user).where(created_at: Date.today.beginning_of_day..Date.today.end_of_day) # 全ユーザーの投稿を表示
   end
 end
