80869c817d289f291d605a574a51f4434ca96b17
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-15 23:56:40 +0900

    all_postsでの表示日付降順に並べる&レイアウト修正

--- app/controllers/users/all_posts_controller.rb
+++ app/controllers/users/all_posts_controller.rb
@@ -2,6 +2,6 @@ class Users::AllPostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.includes(:user).where(created_at: Date.today.beginning_of_day..Date.today.end_of_day) # 全ユーザーの投稿を表示
+    @posts = Post.includes(:user).where(created_at: Date.today.beginning_of_day..Date.today.end_of_day).order(created_at: :desc) # 全ユーザーの投稿を表示
   end
 end
