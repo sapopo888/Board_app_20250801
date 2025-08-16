f9d9ff06a1af34a53bdd493319e349bb8d3f435d
sapopo888 <sapopo0813@yahoo.co.jp>
2025-08-16 14:47:11 +0900

    PageFlipオプション追加

--- app/javascript/pageflip.js
+++ app/javascript/pageflip.js
@@ -1,9 +1,10 @@
   import { PageFlip } from "page-flip";
 
-
   const pageFlip = new PageFlip(document.getElementById('book'), {
     width: 400,
     height: 600,
+    showCover: true,
+    startPage: 1,
   });
 
   pageFlip.loadFromHTML(document.querySelectorAll('.post'));
